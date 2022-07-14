3f7be2afeb4e0b63b150210aac76ab5a0b191d5f
Charles Frye <charlesfrye@berkeley.edu>
2022-07-14 15:34:01 -0700

    sets up repo subsetting for Labs 01 and 02

--- text_recognizer/callbacks/logging/__init__.py
+++ text_recognizer/callbacks/logging/__init__.py
@@ -1,6 +1,9 @@
-from . import imtotext, model
+from . import model
+
+# Hide lines below until Lab 04
+from . import imtotext
 from .imtotext import ImageToTextTableLogger as ImageToTextLogger
+
+# Hide lines above until Lab 04
 from .model import ModelSizeLogger
 from .optim import LearningRateMonitor
-
-__all__ = ["imtotext", "model", "ModelSizeLogger", "ImageToTextLogger", "LearningRateMonitor"]
