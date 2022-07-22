987d443e84e9a639a35fe2cf4e3670681a7bbb95
Charles Frye <charlesfrye@berkeley.edu>
2022-07-21 23:03:21 -0700

    updates code-hiding for models __init__

--- text_recognizer/models/__init__.py
+++ text_recognizer/models/__init__.py
@@ -3,21 +3,11 @@ from .mlp import MLP
 
 # Hide lines below until Lab 02
 from .cnn import CNN
+from .line_cnn_simple import LineCNNSimple
 
 # Hide lines above until Lab 02
 
 # Hide lines below until Lab 03
-from .line_cnn_simple import LineCNNSimple
-from .line_cnn import LineCNN
-
-# Hide lines above until Lab 03
-
-# Hide lines below until Lab 04
-from .line_cnn_transformer import LineCNNTransformer
-
-# Hide lines above until Lab 04
-
-# Hide lines below until Lab 07
 from .resnet_transformer import ResnetTransformer
 
-# Hide lines above until Lab 07
+# Hide lines above until Lab 03
