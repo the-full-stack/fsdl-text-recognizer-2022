cb9bd40b308d00553efe4903f5ceb5219198c1e5
Charles Frye <charlesfrye@berkeley.edu>
2022-07-23 17:55:27 -0700

    copies notebook tester to labs and renames "lab00"

--- tasks/notebook_test.sh
+++ tasks/notebook_test.sh
@@ -2,12 +2,15 @@
 set -uo pipefail
 set +e
 
-# provide an escaped regex pattern, e.g. "99_\|03_", to override
-# use /(\?\!)/, an unmatchable regex (from https://codegolf.stackexchange.com/questions/18393), to match all
-IGNORE_PATTERN="${1:-99_}"
+# provide escaped regex patterns, e.g. "99_\|03_", as CLI args to override these
+SELECT_PATTERN="${1:-.*}"
+IGNORE_PATTERN="${2:-99_}"
+# to match all, use ".*" and "\$-" (see discussion at https://stackoverflow.com/questions/2930182/regex-to-not-match-any-characters)
+# use https://regex101.com/r/yjcs1Z/3 to an interactive regex testing tool
 
 # use environment variables to control notebook bootstrapping behavior, see notebook setup cells and https://fsdl.me/bootstrap-gist
 FSDL_REPO="$(basename "$(pwd)")"
 export FSDL_REPO
 
-find notebooks -maxdepth 1 | grep \.ipynb$ | grep -v nbconvert | grep -v "$IGNORE_PATTERN" | xargs jupyter nbconvert --to notebook --execute
+# look inside notebooks dir for .ipynbs that are not nbconvert files and that match the pattern and not the ignore and pass them to nbconvert to run
+find notebooks -maxdepth 1 | grep \.ipynb$ | grep -v nbconvert | grep "$SELECT_PATTERN" | grep -v "$IGNORE_PATTERN" | xargs jupyter nbconvert --to notebook --execute
