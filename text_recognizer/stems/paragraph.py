e6cce78ed3f8efb65514bbae02cd34095141672c
srbhchandra <srbhchandra@gmail.com>
2022-07-26 23:20:22 -0700

    code-formating

--- text_recognizer/stems/paragraph.py
+++ text_recognizer/stems/paragraph.py
@@ -18,13 +18,15 @@ class ParagraphStem(ImageStem):
 
     scale_factor = IMAGE_SCALE_FACTOR
 
-    def __init__(self,
-                 augment=False,
-                 color_jitter_kwargs=None,
-                 random_affine_kwargs=None,
-                 random_perspective_kwargs=None,
-                 gaussian_blur_kwargs=None,
-                 sharpness_kwargs=None):
+    def __init__(
+        self,
+        augment=False,
+        color_jitter_kwargs=None,
+        random_affine_kwargs=None,
+        random_perspective_kwargs=None,
+        gaussian_blur_kwargs=None,
+        sharpness_kwargs=None,
+    ):
         super().__init__()
 
         if not augment:
@@ -46,15 +48,9 @@ class ParagraphStem(ImageStem):
                     "interpolation": transforms.InterpolationMode.BILINEAR,
                 }
             if gaussian_blur_kwargs is None:
-                gaussian_blur_kwargs = {
-                    "kernel_size": (3, 3),
-                    "sigma": (0.1, 1.0)
-                }
+                gaussian_blur_kwargs = {"kernel_size": (3, 3), "sigma": (0.1, 1.0)}
             if sharpness_kwargs is None:
-                sharpness_kwargs = {
-                    "sharpness_factor": 2,
-                    "p": 0.5
-                }
+                sharpness_kwargs = {"sharpness_factor": 2, "p": 0.5}
 
             # IMAGE_SHAPE is (576, 640)
             self.pil_transforms = transforms.Compose(
@@ -66,7 +62,7 @@ class ParagraphStem(ImageStem):
                     transforms.RandomAffine(**random_affine_kwargs),
                     transforms.RandomPerspective(**random_perspective_kwargs),
                     transforms.GaussianBlur(**gaussian_blur_kwargs),
-                    transforms.RandomAdjustSharpness(**sharpness_kwargs)
+                    transforms.RandomAdjustSharpness(**sharpness_kwargs),
                 ]
             )
 
