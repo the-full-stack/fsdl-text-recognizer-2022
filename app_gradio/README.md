53632102ccb8fc494faa11cdde0a29644389f896
Charles Frye <charlesfrye@berkeley.edu>
2022-07-28 21:58:06 -0700

    adds Gantry-backed logging to the text-recognizer

---
+++ app_gradio/README.md
@@ -0,0 +1,13 @@
+## Full-Paragraph Optical Character Recognition
+
+For more on how this application works,
+[check out the GitHub repo](https://github.com/full-stack-deep-learning/fsdl-text-recognizer-2022).
+
+### Flagging
+
+If the model outputs in the top-right are wrong in some way,
+let us know by clicking the "flagging" buttons underneath.
+
+We'll analyze the results with
+[Gantry](https://gantry.io/blog/introducing-gantry/)
+and use them to improve the model!
--- app_gradio/app.py
+++ app_gradio/app.py
@@ -4,22 +4,30 @@ import json
 import logging
 import os
 from pathlib import Path
-from time import time
-from uuid import uuid4
 
 import gradio as gr
 from PIL import ImageStat
 import requests
 
+# Hide lines below until Lab 08
+from app_gradio.flagging import GantryImageToTextLogger
+from app_gradio.s3_util import make_unique_bucket_name
+
+# Hide lines above until Lab 08
 from text_recognizer.paragraph_text_recognizer import ParagraphTextRecognizer
 import text_recognizer.util as util
 
 os.environ["CUDA_VISIBLE_DEVICES"] = ""  # do not use GPU
+# Hide lines below until Lab 08
+GANTRY_API_KEY = os.environ.get("GANTRY_API_KEY")
+APP_NAME = "fsdl-text-recognizer"
+# Hide lines above until Lab 08
 
 logging.basicConfig(level=logging.INFO)
-LOG_DIR = Path("logs")  # where do we log images?
-IMAGE_LOG_DIR = LOG_DIR / "images"
-IMAGE_LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+APP_DIR = Path(__file__).resolve().parent  # what is the directory for this application?
+FAVICON = APP_DIR / "1f95e.png"  # path to a small image for display in browser tab and social media
+README = APP_DIR / "README.md"  # path to an app readme file in HTML/markdown
 
 DEFAULT_PORT = 11700
 
@@ -30,8 +38,8 @@ def main(args):
     frontend.launch(
         server_name="0.0.0.0",  # make server accessible, binding all interfaces  # noqa: S104
         server_port=args.port,  # set a port to bind to, failing if unavailable
-        share=True,  # should we create a (temporary) public link on gradio.app?
-        favicon_path="app_gradio/1f95e.png",  # what icon should we display in the address bar?
+        share=True,  # should we create a (temporary) public link on https://gradio.app?
+        favicon_path=FAVICON,  # what icon should we display in the address bar?
     )
 
 
@@ -42,22 +50,25 @@ def make_frontend(fn):
 
     examples = [[str(path)] for path in example_paths]
 
-    readme = """# Text Recognizer"""
+    readme = _load_readme()
 
     # build a basic browser interface to a Python function
     frontend = gr.Interface(
         fn=fn,  # which Python function are we interacting with?
         outputs="text",  # what output widgets does it need? the default text widget
         # what input widgets does it need? we configure an image widget
-        inputs=gr.inputs.Image(type="pil", invert_colors=False, label="Handwritten Text"),
-        theme="default",  # what theme should we use? provide css kwarg to set your own style
+        inputs=gr.inputs.Image(type="pil", label="Handwritten Text"),
         title="📝 Text Recognizer",  # what should we display at the top of the page?
-        thumbnail="app_gradio/1f95e.png",  # what should we display when the link is shared, e.g. on social media?
+        thumbnail=FAVICON,  # what should we display when the link is shared, e.g. on social media?
         description=__doc__,  # what should we display just above the interface?
         article=readme,  # what long-form content should we display below the interface?
         examples=examples,  # which potential inputs should we provide?
         cache_examples=False,  # should we cache those inputs for faster inference? slows down start
-        flagging_options=["incorrect", "unusable", "offensive", "other"],  # what options do users have for feedback?
+        # Hide lines below until Lab 08
+        flagging_options=["incorrect", "offensive", "other"],  # what options do users have for feedback?
+        flagging_callback=GantryImageToTextLogger(application=APP_NAME, api_key=GANTRY_API_KEY),
+        flagging_dir=make_unique_bucket_name(prefix=APP_NAME, seed=GANTRY_API_KEY)
+        # Hide lines above until Lab 08
     )
 
     return frontend
@@ -66,8 +77,7 @@ def make_frontend(fn):
 class PredictorBackend:
     """Interface to a backend that serves predictions.
 
-    To communicate with a backend accessible via a URL,
-    provide the url kwarg.
+    To communicate with a backend accessible via a URL, provide the url kwarg.
 
     Otherwise, runs a predictor locally.
     """
@@ -88,15 +98,12 @@ class PredictorBackend:
     def _predict_with_metrics(self, image):
         pred = self._predict(image)
 
-        fname = IMAGE_LOG_DIR / f"{int(time())}-{uuid4()}.png"
-        image.save(fname)
         stats = ImageStat.Stat(image)
         metrics = {
             "image_mean_intensity": stats.mean,
             "image_median": stats.median,
             "image_extrema": stats.extrema,
             "image_area": image.size[0] * image.size[1],
-            "image_filename": fname,
             "pred_length": len(pred),
         }
         return pred, metrics
@@ -118,13 +125,19 @@ class PredictorBackend:
         logging.info(f"PRED >begin\n{pred}\nPRED >end")
 
 
+def _load_readme():
+    with open(README) as f:
+        readme = "".join(f.readlines())
+    return readme
+
+
 def _make_parser():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "--model_url",
         default=None,
         type=str,
-        help="Identifies a URL to which to send image data. Data is base64-encoded, converted to a utf-8 string, and then set via a POST request. Default is None, which instead sends the data to a model created locally.",
+        help="Identifies a URL to which to send image data. Data is base64-encoded, converted to a utf-8 string, and then set via a POST request as JSON with the key 'image'. Default is None, which instead sends the data to a model running locally.",
     )
     parser.add_argument(
         "--port",
