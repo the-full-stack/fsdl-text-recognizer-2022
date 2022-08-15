0769044f0b9acf4fbc836e3cfc53570865e23829
Charles Frye <charlesfrye@berkeley.edu>
2022-08-14 18:21:45 -0700

    preps for lab04

--- text_recognizer/callbacks/__init__.py
+++ text_recognizer/callbacks/__init__.py
@@ -1,8 +1,5 @@
 from .model import ModelSizeLogger
 from .optim import LearningRateMonitor
 
-# Hide lines below until Lab 04
 from . import imtotext
 from .imtotext import ImageToTextTableLogger as ImageToTextLogger
-
-# Hide lines above until Lab 04
