ef0e3ad6879529ae6663ff4800c2b254baf95d80
Charles Frye <cfrye59@gmail.com>
2022-08-16 16:13:00 -0700

    adds lab04 on experiment management (#48)
    * switches LineCNNTransformer to teacher-in-litmodel style
    
    * tighter estimate of iam_lines max width
    
    * preps for lab04
    
    * adds complete code and draft text for lab04
    
    * fixes display name on Colab
    
    * adds table row for lab04
    
    * reorganizes, adds multi-GPU sweep trick
    
    * switch to management instead of tracking
    
    * polishes lab04, adds better text
    
    * flips bit
    
    * fixes typo

--- text_recognizer/metadata/iam_lines.py
+++ text_recognizer/metadata/iam_lines.py
@@ -5,9 +5,9 @@ PROCESSED_DATA_DIRNAME = shared.DATA_DIRNAME / "processed" / "iam_lines"
 
 IMAGE_SCALE_FACTOR = 2
 
-CHAR_WIDTH = emnist.INPUT_SHAPE[0] // IMAGE_SCALE_FACTOR
+CHAR_WIDTH = emnist.INPUT_SHAPE[0] // IMAGE_SCALE_FACTOR  # rough estimate
 IMAGE_HEIGHT = 112 // IMAGE_SCALE_FACTOR
-IMAGE_WIDTH = 4096 // IMAGE_SCALE_FACTOR  # rounding up IAMLines empirical maximum width to a power of two
+IMAGE_WIDTH = 3072 // IMAGE_SCALE_FACTOR  # rounding up IAMLines empirical maximum width
 
 DIMS = (1, IMAGE_HEIGHT, IMAGE_WIDTH)
 OUTPUT_DIMS = (89, 1)
