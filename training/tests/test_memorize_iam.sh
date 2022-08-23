b9a71661172419fe762af623f3570160b9add4f3
Charles Frye <charlesfrye@berkeley.edu>
2022-08-23 13:22:25 -0700

    rename overfit tests to memorize, clean up

--- training/tests/test_memorize_iam.sh
+++ training/tests/test_memorize_iam.sh
@@ -7,15 +7,16 @@ set +e
 
 FAILURE=false
 
-# constants and CLI args set by aiming for <5 min test on commodity GPU
-MAX_EPOCHS="${1:-100}"
-CRITERION="${2:-2.0}"
+# constants and CLI args set by aiming for <5 min test on commodity GPU,
+#   including data download step
+MAX_EPOCHS="${1:-100}"  # syntax for basic optional arguments in bash
+CRITERION="${2:-1.0}"
 
 # train on GPU if it's available
 GPU=$(python -c 'import torch; print(int(torch.cuda.is_available()))')
 
 python ./training/run_experiment.py \
-  --data_class=IAMOriginalAndSyntheticParagraphs --model_class=ResnetTransformer --loss=transformer \
+  --data_class=IAMParagraphs --model_class=ResnetTransformer --loss=transformer \
   --limit_test_batches 0.0 --overfit_batches 1 --num_sanity_val_steps 0 \
   --augment_data false --tf_dropout 0.0 \
   --gpus "$GPU" --precision 16 --batch_size 16 --lr 0.0001 \
@@ -24,8 +25,8 @@ python ./training/run_experiment.py \
 python -c "import json; loss = json.load(open('training/logs/wandb/latest-run/files/wandb-summary.json'))['train/loss']; assert loss < $CRITERION" || FAILURE=true
 
 if [ "$FAILURE" = true ]; then
-  echo "Overfitting test failed"
+  echo "Memorization test failed at loss criterion $CRITERION"
   exit 1
 fi
-echo "Overfitting test passed"
+echo "Memorization test passed at loss criterion $CRITERION"
 exit 0
