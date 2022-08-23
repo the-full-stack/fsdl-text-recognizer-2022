f3e2845a351fa505c1d2015a4fb9232873e5e300
Charles Frye <charlesfrye@berkeley.edu>
2022-08-23 13:11:26 -0700

    updates line hiding

--- text_recognizer/metadata/iam_paragraphs.py
+++ text_recognizer/metadata/iam_paragraphs.py
@@ -7,9 +7,9 @@ PROCESSED_DATA_DIRNAME = shared.DATA_DIRNAME / "processed" / "iam_paragraphs"
 NEW_LINE_TOKEN = "\n"
 MAPPING = [*emnist.MAPPING, NEW_LINE_TOKEN]
 
-# Hide lines below until Lab 05
+# Hide lines below until Lab 06
 # must match IMAGE_SCALE_FACTOR for IAMLines to be compatible with synthetic paragraphs
-# Hide lines above until Lab 05
+# Hide lines above until Lab 06
 IMAGE_SCALE_FACTOR = 2
 IMAGE_HEIGHT, IMAGE_WIDTH = 576, 640
 IMAGE_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH)
