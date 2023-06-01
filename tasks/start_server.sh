d6330a8217321ad3c13d6bf1522b37dd92f5a0fd
Charles Frye <cfrye59@gmail.com>
2023-06-01 22:22:21 +0000

    lints, allows configuration of NGROK_HOSTNAME

--- tasks/start_server.sh
+++ tasks/start_server.sh
@@ -24,12 +24,12 @@ else
     sudo apt install awscli
 fi
 
-aws configure set default.region $REGION
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key  $AWS_SECRET_ACCESS_KEY
+aws configure set default.region "$AWS_DEFAULT_REGION"
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key  "$AWS_SECRET_ACCESS_KEY"
 
 # authenticate to AWS Elastc Container Registry
-aws ecr get-login-password --region $REGION | sudo docker login --username AWS --password-stdin $AWS_ACCT_ID.dkr.ecr.$REGION.amazonaws.com
+aws ecr get-login-password --region "$AWS_DEFAULT_REGION" | sudo docker login --username AWS --password-stdin "$AWS_ACCT_ID".dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com
 
 # Create a new tmux session
 tmux new-session -d -s $SESSION
@@ -38,11 +38,11 @@ tmux new-session -d -s $SESSION
 tmux split-window -t $SESSION:0 -h
 
 # Run the frontend server in the first pane
-tmux send-keys -t "$SESSION":0.0 "sudo docker run -it -p $PORT:11700 --rm --env-file ~/.server.env $AWS_ACCT_ID.dkr.ecr.$REGION.amazonaws.com/fsdl-text-recognizer/frontend:latest --port 11700 --flagging --gantry --model_url $LAMBDA_URL" Enter
+tmux send-keys -t "$SESSION":0.0 "sudo docker run -it -p $PORT:11700 --rm --env-file ~/.server.env $AWS_ACCT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/fsdl-text-recognizer/frontend:latest --port 11700 --flagging --gantry --model_url $LAMBDA_URL" Enter
 
 # Run the ngrok tunnel in the second pane
-ngrok config add-authtoken $NGROK_AUTHTOKEN
-tmux send-keys -t "$SESSION":0.1 "ngrok --region=us --hostname=fsdl-text-recognizer.ngrok.io http $PORT" Enter
+ngrok config add-authtoken "$NGROK_AUTHTOKEN"
+tmux send-keys -t "$SESSION":0.1 "ngrok --region=us --hostname=$NGROK_HOSTNAME.ngrok.io http $PORT" Enter
 
 # Focus on the first pane
 tmux select-pane -t "$SESSION":0.0
