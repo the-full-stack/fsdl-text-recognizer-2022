63d11cf3cc5d054d4881a230ab1bb72d772623d3
Charles Frye <cfrye59@gmail.com>
2023-06-01 15:52:18 -0700

    more fully automates the frontend server startup on EC2 (#73)
    * improving server startup experience
    
    * switches to AWS config via environmment vars
    
    * lints, allows configuration of NGROK_HOSTNAME
    
    * runs from scratch automatically

--- tasks/start_server.sh
+++ tasks/start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Copy this and .server.env to an EC2 instance and run it to launch the Text Recognizer server
+set -euo pipefail
+IFS=$'\n\t'
 
 if [ -f ~/.server.env ]; then
   # shellcheck disable=SC1090
@@ -9,6 +11,32 @@ fi
 SESSION="fsdl-text-recognizer"
 PORT=11717
 
+if command -v docker &> /dev/null; then
+    echo "Docker is installed."
+else
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
+
+if command -v aws &> /dev/null; then
+    echo "AWS CLI is installed."
+else
+    sudo apt-get install --assume-yes awscli
+fi
+
+if command -v ngrok &> /dev/null; then
+    echo "ngrok is installed."
+else
+    curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt-get install --assume-yes ngrok
+fi
+
+aws configure set default.region "$AWS_DEFAULT_REGION"
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key  "$AWS_SECRET_ACCESS_KEY"
+
+# authenticate to AWS Elastc Container Registry
+aws ecr get-login-password --region "$AWS_DEFAULT_REGION" | sudo docker login --username AWS --password-stdin "$AWS_ACCT_ID".dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com
+
 # Create a new tmux session
 tmux new-session -d -s $SESSION
 
@@ -16,10 +44,11 @@ tmux new-session -d -s $SESSION
 tmux split-window -t $SESSION:0 -h
 
 # Run the frontend server in the first pane
-tmux send-keys -t "$SESSION":0.0 "sudo docker run -it -p $PORT:11700 --rm --env-file .env $AWS_ACCT_ID.dkr.ecr.$REGION.amazonaws.com/fsdl-text-recognizer/frontend:latest --port 11700 --flagging --gantry --model_url $LAMBDA_URL" Enter
+tmux send-keys -t "$SESSION":0.0 "sudo docker run -it -p $PORT:11700 --rm --env-file ~/.server.env $AWS_ACCT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/fsdl-text-recognizer/frontend:latest --port 11700 --flagging --gantry --model_url $LAMBDA_URL" Enter
 
 # Run the ngrok tunnel in the second pane
-tmux send-keys -t "$SESSION":0.1 "ngrok --region=us --hostname=fsdl-text-recognizer.ngrok.io http $PORT" Enter
+ngrok config add-authtoken "$NGROK_AUTHTOKEN"
+tmux send-keys -t "$SESSION":0.1 "ngrok --region=us --hostname=$NGROK_HOSTNAME.ngrok.io http $PORT" Enter
 
 # Focus on the first pane
 tmux select-pane -t "$SESSION":0.0
